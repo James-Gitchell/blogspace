489bc99d82692ceba0fa1d10e98485135cafdca8
James Gitchell <jamesgitchell.dev@gmail.com>
2022-07-22 23:51:36 -0400

    changes to res

--- index.js
+++ index.js
@@ -2,4 +2,7 @@
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts", {method: "GET"})
      .then(res => res.json())
-     .then(data => console.log(data.slice(0,5)))
+     .then(data => {
+        const postArr = data.slice(0,5)
+        console.log(postArr);
+      })
